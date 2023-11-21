815e1a048a7f7ca43a58acdb985771516a05d3e9
Anurag Yadav <yadavraganu@gmail.com>
2023-11-21 11:36:18 +0530

    selection sort

---
+++ sorting/SelectionSort.py
@@ -0,0 +1,11 @@
+# Complexity : O(n2)
+# Space Complexity : O(1)
+class SelectionSort:
+    def sort(self, lst: list[int]) -> list[int]:
+        for i in range(len(lst)):
+            min_index=i
+            for j in range(i+1,len(lst)):
+                if lst[j]<lst[min_index]:
+                    min_index=j
+            lst[i],lst[min_index]=lst[min_index],lst[i]      
+        return lst  
